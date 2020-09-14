92f088157d2339820a7a711d2494079f372f60d5
Alex Bruns <alex.h.bruns@gmail.com>
2020-09-14 00:18:34 -0500

    AHB :: forestry sync

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
-export default function IndexPage() {
-  return <div>now build your stuff</div>;
+import data from "@forestry/pages/home.json";
+
+export default function AboutMePage() {
+  return <pre className="p-4 text-xs">{JSON.stringify(data, null, 2)}</pre>;
 }
